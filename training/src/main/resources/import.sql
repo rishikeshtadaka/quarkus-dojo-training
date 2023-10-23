3bafee8743c677f0925e23e80be5e033ca7661f3
Rishikesh Tadka <rishikesh.10689693@ltimindtree.com>
2023-10-23 08:16:06 +0530

    reset sequence id for customer

--- training/src/main/resources/import.sql
+++ training/src/main/resources/import.sql
@@ -1,6 +1,6 @@
 -- This file allow to write SQL commands that will be emitted in test and dev.
 -- The commands are commented as their support depends of the database
-insert into customer ( id,firstName,lastName,address) values(nextval('customerIdSequence'),'fn-1','ln-1','addr');
+insert into customer ( id,firstName,lastName,address) values(1,'fn-1','ln-1','addr');
 -- insert into myentity (id, field) values(2, 'field-2');
 -- insert into myentity (id, field) values(3, 'field-3');
--- alter sequence myentity_seq restart with 4;
+alter sequence customer_seq restart with 2;
