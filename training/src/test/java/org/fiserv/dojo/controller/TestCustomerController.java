7774acb63dca1b2c97330fd45fce5ca17ebaa32e
Rishikesh Tadka <rishikesh.10689693@ltimindtree.com>
2023-10-23 10:46:26 +0530

    Added test cases for ADD, update delete

--- training/src/test/java/org/fiserv/dojo/controller/TestCustomerController.java
+++ training/src/test/java/org/fiserv/dojo/controller/TestCustomerController.java
@@ -1,6 +1,9 @@
 package org.fiserv.dojo.controller;
 
 import io.quarkus.test.junit.QuarkusTest;
+import jakarta.json.Json;
+import jakarta.json.JsonObject;
+import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 import org.junit.jupiter.api.*;
 
@@ -41,16 +44,42 @@ public class TestCustomerController {
     @Test
     @Order(3)
     void CreateCustomer(){
-
+        JsonObject jsonObject= Json.createObjectBuilder()
+                .add("firstName","fn3")
+                .add("lastName","ln3")
+                .add("address","addr3")
+                .build();
+        given()
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(jsonObject.toString())
+                .when()
+                .post("/customers")
+                .then()
+                .statusCode(Response.Status.CREATED.getStatusCode());
     }
     @Test
     @Order(4)
     void UpdateCustomer(){
-
+        JsonObject jsonObject= Json.createObjectBuilder()
+                .add("firstName","updated")
+                .add("lastName","ln3")
+                .add("address","addr3")
+                .build();
+        given()
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(jsonObject.toString())
+                .when()
+                .put("/customers/1")
+                .then()
+                .statusCode(Response.Status.OK.getStatusCode());
     }
     @Test
     @Order(5)
     void DeleteCustomer(){
-
+        given()
+                .when()
+                .delete("/customers/1")
+                .then()
+                .statusCode(Response.Status.OK.getStatusCode());
     }
 }
