6d4a28213874ccc97fed37e32b5177fa4c8781e3
Rishikesh Tadka <rishikesh.10689693@ltimindtree.com>
2023-10-23 23:12:09 +0530

    liquibase configurations

--- training/src/main/java/org/fiserv/dojo/Exception/GenericErrorHandling.java
+++ training/src/main/java/org/fiserv/dojo/Exception/GenericErrorHandling.java
@@ -1,5 +1,6 @@
 package org.fiserv.dojo.Exception;
 
+import io.quarkus.logging.Log;
 import jakarta.ws.rs.InternalServerErrorException;
 import jakarta.ws.rs.core.Response;
 import jakarta.ws.rs.ext.ExceptionMapper;
@@ -10,6 +11,7 @@ public class GenericErrorHandling implements ExceptionMapper<Exception> {
 
     @Override
     public Response toResponse(Exception e) {
+        Log.error(e.getMessage());
         return Response.status(Response.Status.BAD_REQUEST).entity("Generic:Something went wrong").build();
     }
 }
--- training/src/main/java/org/fiserv/dojo/domain/Customer.java
+++ training/src/main/java/org/fiserv/dojo/domain/Customer.java
@@ -3,17 +3,18 @@ package org.fiserv.dojo.domain;
 
 import io.quarkus.hibernate.orm.panache.PanacheEntity;
 import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
-public class Customer extends PanacheEntity {
+public class Customer extends PanacheEntityBase {
+    @Id
+    @SequenceGenerator(name="customer_seq",sequenceName="customer_seq",allocationSize=1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "customer_seq")
+    public Long id;
     @Column(length = 100)
     public String FirstName;
     @Column(length = 100)
